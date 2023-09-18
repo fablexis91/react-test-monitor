f52e23c6c7b1e5ce637bf68675cb18c0ab7728fc
Fabian Pernia <fabian.pernia@endava.com>
2023-09-18 14:53:32 -0500

    raygun config

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import rg4js from 'raygun4js';
+
+
+
 function App() {
   const [count, setCount] = useState(0)
+
+  rg4js('apiKey', 'ofvz8BV6EnCAPASEHEWSA')
+  rg4js('enableCrashReporting', true)
+  rg4js('setUser', {
+    identifier: '662607004',
+    isAnonymous: false,
+    email: 'fabian.pernia@endava.com',
+    firstName: 'Fabian',
+    fullName: 'Fabian Pernia',
+  })
 
   return (
     <>
@@ -24,6 +38,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+
+        <h1>This is a test that's gonna fail: Click the button below</h1>
+        <button onClick={() => methodDoesNotExist()}>Break the world</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
