d7da482bc3bdbb07726e2633b946ac6b3a400e46
Fabian Pernia <fabian.pernia@endava.com>
2023-09-18 11:46:25 -0500

    sentry config

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as Sentry from "@sentry/browser";
 
 function App() {
   const [count, setCount] = useState(0)
+  Sentry.setUser({ email: "fabian.pernia@endava.com" });
 
   return (
     <>
@@ -24,6 +26,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+
+        <h1>This is a test that's gonna fail: Click the button below</h1>
+        <button onClick={() => methodDoesNotExist()}>Break the world</button>;
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://becdaf01bc5c4b3c18a78ac3a8595ef4@o4505902232895488.ingest.sentry.io/4505902364295168",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
